c1609db8ed15ed20c172817036743cabd986e1d1
Anna <konovalyashka@yandex.ru>
2023-08-09 18:33:41 +0300

    обработка MissingServletRequestParameterException в stats-server

--- stats-service/stats-server/src/main/java/ru/practicum/server/exception/ErrorHandler.java
+++ stats-service/stats-server/src/main/java/ru/practicum/server/exception/ErrorHandler.java
@@ -2,6 +2,7 @@ package ru.practicum.server.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,7 +12,8 @@ import java.util.Map;
 
 @RestControllerAdvice
 public class ErrorHandler {
-    @ExceptionHandler({MethodArgumentNotValidException.class, ConstraintViolationException.class, IllegalArgumentException.class})
+    @ExceptionHandler({MethodArgumentNotValidException.class, ConstraintViolationException.class, IllegalArgumentException.class,
+            MissingServletRequestParameterException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Map<String, String> handleNotValidException(Exception e) {
         return Map.of(
